8c546a3a2c10d519edabda17b21704d179e84cd8
Xavier Cuases <xaviercuases@hotmail.com>
2026-05-10 08:40:22 -0500

    feat: :sparkles: define Categoria model and initialize database schema

--- app/categoria.py
+++ app/categoria.py
@@ -1,9 +1,27 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345@localhost/pysqlapi'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
+class Categoria(db.Model):
+    cat_id = db.Column(db.Integer,primary_key=True)
+    cat_nom = db.Column(db.String(100))
+    cat_desp = db.Column(db.String(100))
+
+    def __init__(self,cat_nom,cat_desp):
+        self.cat_nom = cat_nom
+        self.cat_desp = cat_desp
+
+db.create_all()        
+
+#Mensaje de bienvenida
 @app.route('/',methods=['GET'])
 def index():
     return jsonify({'Mensaje':'Bienvenido'})
@@ -11,4 +29,3 @@ def index():
 if __name__=="__main__":
     app.run(debug=True)
 
-    
